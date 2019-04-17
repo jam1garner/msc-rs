96d6956e6dbadfcd3b41106ae715984e93cd2891
jam1garner <jam1.mcleod@hotmail.com>
2019-04-17 11:19:58 -0400

    Add write_to_file

--- src/mscb_file/mod.rs
+++ src/mscb_file/mod.rs
@@ -1,7 +1,6 @@
 mod parser;
 mod writer;
 
-use writer::*;
 use super::Script;
 use parser::take_file;
 use std::fs::File;
@@ -16,9 +15,17 @@ pub struct MscsbFile {
 
 impl MscsbFile {
     pub fn open<P: AsRef<Path>>(path: P) -> Option<MscsbFile> {
-        let mut buffer = Vec::new();
+        let mut buffer = vec![];
         File::open(path).ok()?.read_to_end(&mut buffer).ok()?;
         Some(take_file(&buffer[..]).unwrap().1)
+    }
+
+    pub fn write_to_file<P: AsRef<Path>>(&self, path: P) -> std::io::Result<()> {
+        let mut buffer = vec![];
+        self.write(&mut buffer);
+        File::create(path)?
+            .write_all(&buffer[..])?;
+        Ok(())
     }
 
     pub fn iter(&self) -> std::slice::Iter<Script> {
